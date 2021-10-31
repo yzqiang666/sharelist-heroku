89bae039467b1e2546184cbdd70c824af71a371d
yzqiang666 <yzqiang6@gmail.com>
2021-10-31 12:36:20 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,11 @@ unzip sharelist.zip
 
 cd /app/sharelist-master
 npm install
-
+mkdir -p /app/sharelist-master/cache
 if echo "$CONFIG" | grep -q -i "^http"; then
   wget --no-check-certificate $CONFIG -O /app/sharelist-master/cache/config.json
 else
   echo -e "$CONFIG" > /app/sharelist-master/cache/config.json
 fi
-
+cat /app/sharelist-master/cache/config.json
 npm start
